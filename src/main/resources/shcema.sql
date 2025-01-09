4388fc234c96fbb751859ad1426247cc46b1417e
mingwan <ibini12@gmail.com>
2025-01-09 16:34:56 +0900

    [feat] 🐢 login 유저 설정

---
+++ src/main/resources/shcema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS User(
+    user_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(127) NOT NULL,
+    user_gender VARCHAR(1) NOT NULL,
+    user_email VARCHAR(127) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,
+    user_birth VARCHAR(8) NOT NULL,
+    user_registered_date DATE NOT NULL,
+    user_kakao_id VARCHAR(255)
+);
