cf6c479545ed219494ad4a96575e4cb33952b042
hyemch <blossoming33@gmail.com>
2025-01-10 10:17:24 +0900

    [feat] 🧚🏻‍♀️ Global Exception Handler 추가 #2

---
+++ src/main/java/com/example/lifeonhana/global/exception/BaseException.java
@@ -0,0 +1,19 @@
+package com.example.lifeonhana.global.exception;
+
+import org.aspectj.bridge.Message;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+import lombok.Getter;
+
+@Getter
+public abstract class BaseException extends RuntimeException {
+	protected BaseException(String message) {
+		super(message);
+	}
+	public abstract HttpStatusCode getStatusCode();
+	public abstract HttpStatus getHttpStatus();
+	public String getCustomMessage() {
+		return getMessage();
+	};
+}
