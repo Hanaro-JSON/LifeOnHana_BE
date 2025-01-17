aa42f89cd1a11fcc2924157ef140a7fce099f282
yena <yeena114@naver.com>
2025-01-17 11:02:49 +0900

    [feat] 🦧 swagger 설정

--- src/main/java/com/example/lifeonhana/controller/ArticleLikeController.java
+++ src/main/java/com/example/lifeonhana/controller/ArticleLikeController.java
@@ -14,15 +14,19 @@ import com.example.lifeonhana.dto.response.LikeResponseDto;
 import com.example.lifeonhana.service.ArticleLikeService;
 import com.example.lifeonhana.service.JwtService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/articles")
 @RequiredArgsConstructor
+@Tag(name="ArticleLike API", description = "칼럼 좋아요 관련 api")
 public class ArticleLikeController {
 	private final JwtService jwtService;
 	private final ArticleLikeService articleLikeService;
 
+	@Operation(summary = "칼럼 좋아요 생성 && 취소", description = "칼럼 좋아요 생성 && 취소")
 	@PostMapping("/{articleId}/like")
 	public ResponseEntity<ApiResult> toggleLike(
 		@PathVariable Long articleId,
@@ -42,6 +46,8 @@ public class ArticleLikeController {
 				.build()
 		);
 	}
+
+	@Operation(summary = "칼럼 좋아요 수 && 여부 조회", description = "칼럼 좋아요 수 && 여부 조회")
 	@GetMapping("/{articleId}/like")
 	public ResponseEntity<ApiResult> getLikeInfo(
 		@PathVariable Long articleId,
