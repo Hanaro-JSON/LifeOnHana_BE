e08c9628ef8d381c77e7dfccab2ed884b2a2f876
teadmu <dharmavarsa@naver.com>
2025-01-20 16:28:15 +0900

    [feat] 🦥 칼럼 검색 기능

--- src/main/java/com/example/lifeonhana/repository/ArticleRepository.java
+++ src/main/java/com/example/lifeonhana/repository/ArticleRepository.java
@@ -2,20 +2,21 @@ package com.example.lifeonhana.repository;
 
 import java.util.List;
 
-import com.example.lifeonhana.entity.enums.ArticleCategory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.example.lifeonhana.entity.Article;
 
 @Repository
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, Long> , JpaSpecificationExecutor<Article> {
 	Slice<Article> findByArticleIdIn(List<Long> articleIds, Pageable pageable);
 
 	Slice<Article> findByArticleIdInAndCategory(List<Long> articleIds, Article.Category category, Pageable pageable);
 
 	Page<Article> findByCategory(Article.Category category, Pageable pageable);
 }
+
