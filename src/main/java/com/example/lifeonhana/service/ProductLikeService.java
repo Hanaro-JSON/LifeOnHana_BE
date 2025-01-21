32b12f2770e34486200965fecc24cb31519c2ccf
hyemch <blossoming33@gmail.com>
2025-01-21 09:19:45 +0900

    [fix] 🧚🏻‍♀️좋아요한 상품 목록 responseDTO 수정 #55

--- src/main/java/com/example/lifeonhana/service/ProductLikeService.java
+++ src/main/java/com/example/lifeonhana/service/ProductLikeService.java
@@ -8,7 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.example.lifeonhana.dto.response.ProductResponseDTO;
-import com.example.lifeonhana.dto.response.ProductLikeResponseDTO;
+import com.example.lifeonhana.dto.response.ProductListResponseDTO;
 import com.example.lifeonhana.entity.ProductLike;
 import com.example.lifeonhana.repository.ProductLikeRepository;
 
@@ -19,16 +19,16 @@ public class ProductLikeService {
 		this.productLikeRepository = productLikeRepository;
 	}
 
-	public ProductLikeResponseDTO getProductLikes(String authId, Pageable pageable) {
+	public ProductListResponseDTO<ProductResponseDTO> getProductLikes(String authId, Pageable pageable) {
 		Page<ProductLike> productLikes = productLikeRepository.findByUser_AuthIdAndIsLikeTrue(authId, pageable);
 		List<ProductResponseDTO> productList =
 			productLikes.stream().map(like -> ProductResponseDTO.fromEntity(like.getProduct())).collect(Collectors.toList());
 
-		return new ProductLikeResponseDTO(
+		return new ProductListResponseDTO<>(
 			productList
-			,productLikes.getNumber() + 1
-			,productLikes.getSize()
-			,productLikes.getTotalPages()
-			,productLikes.getTotalElements());
+			, productLikes.getNumber() + 1
+			, productLikes.getSize()
+			, productLikes.getTotalPages()
+			, productLikes.getTotalElements());
 	}
 }
