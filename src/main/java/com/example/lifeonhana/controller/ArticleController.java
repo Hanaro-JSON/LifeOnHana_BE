286741875de8bfce4c9576b36b0e00ab56718518
yena <yeena114@naver.com>
2025-01-21 10:37:25 +0900

    [fix] 🦧 리베이스 에러 수정

--- src/main/java/com/example/lifeonhana/controller/ArticleController.java
+++ src/main/java/com/example/lifeonhana/controller/ArticleController.java
@@ -1,6 +1,7 @@
 package com.example.lifeonhana.controller;
 
 import com.example.lifeonhana.dto.response.ArticleListItemResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
