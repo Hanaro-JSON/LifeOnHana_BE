f61b3312b8c0ce0a7c9600320b7d38aad3b76ecc
mingwan <ibini12@gmail.com>
2025-01-21 17:38:35 +0900

    [feat] 🐢 휘릭 api 개발 #45

---
+++ src/main/java/com/example/lifeonhana/dto/response/PageableDTO.java
@@ -0,0 +1,11 @@
+package com.example.lifeonhana.dto.response;
+
+public record PageableDTO(
+	int pageNumber,
+	int pageSize,
+	int totalPages,
+	long totalElements,
+	boolean first,
+	boolean last,
+	boolean empty
+) {}
