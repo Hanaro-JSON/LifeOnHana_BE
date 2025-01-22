1993eae0ef6fcf4732a5dc0bd7ff11e329d1a932
hyemch <blossoming33@gmail.com>
2025-01-22 10:42:51 +0900

    [refactor] 🧚🏻‍♀️상품 상세 좋아요 여부 추가 #77

--- src/main/java/com/example/lifeonhana/service/ProductService.java
+++ src/main/java/com/example/lifeonhana/service/ProductService.java
@@ -14,6 +14,7 @@ import com.example.lifeonhana.dto.response.SavingProductResponseDTO;
 import com.example.lifeonhana.dto.response.SimpleProductResponseDTO;
 import com.example.lifeonhana.entity.Product;
 import com.example.lifeonhana.global.exception.BadRequestException;
+import com.example.lifeonhana.repository.ProductLikeRepository;
 import com.example.lifeonhana.repository.ProductRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -23,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 public class ProductService {
 
 	private final ProductRepository productRepository;
+	private final ProductLikeRepository productLikeRepository;
 
 	public ProductListResponseDTO<SimpleProductResponseDTO> getProducts(String category, int page, int size) {
 		Pageable pageable = PageRequest.of(page - 1, size);
@@ -51,7 +53,8 @@ public class ProductService {
 			throw new BadRequestException("존재하지 않는 id 입니다.");
 		}
 
-		return SavingProductResponseDTO.fromEntity(product);
+		boolean isLike = productLikeRepository.existsById_ProductIdAndIsLikeTrue(productId);
+		return SavingProductResponseDTO.fromEntity(product, isLike);
 	}
 
 	public LoanProductDetailResponseDTO getLoanProduct(Long productId) {
@@ -62,7 +65,8 @@ public class ProductService {
 			throw new BadRequestException("존재하지 않는 id 입니다.");
 		}
 
-		return LoanProductDetailResponseDTO.fromEntity(product);
+		boolean isLike = productLikeRepository.existsById_ProductIdAndIsLikeTrue(productId);
+		return LoanProductDetailResponseDTO.fromEntity(product, isLike);
 	}
 
 	public LifeProductResponseDTO getLifeProduct(Long productId) {
@@ -73,6 +77,7 @@ public class ProductService {
 			throw new BadRequestException("존재하지 않는 id 입니다.");
 		}
 
-		return LifeProductResponseDTO.fromEntity(product);
+		boolean isLike = productLikeRepository.existsById_ProductIdAndIsLikeTrue(productId);
+		return LifeProductResponseDTO.fromEntity(product, isLike);
 	}
 }
