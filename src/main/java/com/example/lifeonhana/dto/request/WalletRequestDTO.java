e16068cf85c90b950b40489bfd59e40e8f2660d5
hyemch <blossoming33@gmail.com>
2025-01-24 17:41:08 +0900

    [fix] 🧚🏻‍♀️목돈 신청 수정 #97

--- src/main/java/com/example/lifeonhana/dto/request/WalletRequestDTO.java
+++ src/main/java/com/example/lifeonhana/dto/request/WalletRequestDTO.java
@@ -3,10 +3,13 @@ package com.example.lifeonhana.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record WalletRequestDTO(
-	@Schema(description = "하나 지갑 금액")
+	@Schema(description = "하나지갑 금액")
 	long walletAmount,
+	@Schema(description = "하나지갑 월급 지급일")
 	String paymentDay,
+	@Schema(description = "시작일")
 	String startDate,
+	@Schema(description = "종료일")
 	String endDate
 ) {
 }
