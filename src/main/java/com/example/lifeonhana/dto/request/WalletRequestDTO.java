88ff173aa3d3f184d3d155ce0a75654de1fac0a9
hyemch <blossoming33@gmail.com>
2025-01-24 10:54:39 +0900

    [feat] 🧚🏻‍♀️하나지갑 amount 스웨거 스키마 추가

--- src/main/java/com/example/lifeonhana/dto/request/WalletRequestDTO.java
+++ src/main/java/com/example/lifeonhana/dto/request/WalletRequestDTO.java
@@ -1,6 +1,9 @@
 package com.example.lifeonhana.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record WalletRequestDTO(
+	@Schema(description = "하나 지갑 금액")
 	long walletAmount,
 	String paymentDay,
 	String startDate,
