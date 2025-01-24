514b5396538a0393df905684fa0f67b9fa48fba4
hyemch <blossoming33@gmail.com>
2025-01-24 10:31:25 +0900

    [feat] 🧚🏻‍♀️하나지갑 id 스웨거 스키마 추가

--- src/main/java/com/example/lifeonhana/dto/response/WalletResponseDTO.java
+++ src/main/java/com/example/lifeonhana/dto/response/WalletResponseDTO.java
@@ -1,6 +1,9 @@
 package com.example.lifeonhana.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record WalletResponseDTO(
+	@Schema(description = "하나지갑 ID")
 	long walletId,
 	long walletAmount,
 	String paymentDay,
