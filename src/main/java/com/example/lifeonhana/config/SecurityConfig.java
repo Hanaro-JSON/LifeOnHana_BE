aa131d3483952be48699d3cf8fb9ebc345322ab8
hyemch <blossoming33@gmail.com>
2025-01-26 03:17:26 +0900

    [fix] 🧚🏻‍♀️cicd 수정

--- src/main/java/com/example/lifeonhana/config/SecurityConfig.java
+++ src/main/java/com/example/lifeonhana/config/SecurityConfig.java
@@ -12,12 +12,19 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 import java.util.Arrays;
 
 import com.example.lifeonhana.filter.JwtAuthenticationFilter;
 import com.example.lifeonhana.service.JwtService;
 import com.example.lifeonhana.service.RedisService;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -40,7 +47,7 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // 프론트엔드 서버 주소
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://life-on-hana-7gzbn8txw-bbzjuns-projects.vercel.app")); // 프론트엔드 서버 주소
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
 		configuration.setAllowCredentials(true);
@@ -61,17 +68,29 @@ public class SecurityConfig {
 			.authorizeHttpRequests(auth -> {
 				auth.requestMatchers(
 						"/api/**",
-						"/swagger-ui/**",
-						"/swagger-ui.html",
-						"/swagger-resources/**",
-						"/api-docs/**",
-						"/v3/api-docs/**",
-						"/actuator/**"
+						"/api/swagger-ui/**",
+						"/api/swagger-ui.html",
+						"/api/swagger-resources/**",
+						"/api/api-docs/**",
+						"/api/v3/api-docs/**",
+						"/api/actuator/**"
 					).permitAll()
 					.anyRequest().authenticated();
 			})
-			.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+			.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+			.addFilterBefore(new SameSiteCookieFilter(),
+				JwtAuthenticationFilter.class);
 
 		return http.build();
 	}
+
+	public static class SameSiteCookieFilter extends OncePerRequestFilter {
+		@Override
+		protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+			FilterChain filterChain) throws IOException, ServletException {
+			response.addHeader("Set-Cookie", "SameSite=None; Secure");
+			filterChain.doFilter(request, response);
+		}
+	}
+
 }
